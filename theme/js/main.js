4980c087aaedb0cc5c735d5d9a5e651cf0ca50b1
Bloodskate <prasannamishra50@gmail.com>
2016-12-11 17:40:39 +0545

    dual navbar

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,11 +1,14 @@
 $(window).scroll(function() {
     var nav = $('#navbarMain');
-    var top = 50;
+    var body = $('#body');
+    var top = 30;
     if ($(window).scrollTop() >= top) {
 
         nav.addClass('navbar-fixed-top');
+        body.addClass('padding-top');
 
     } else {
         nav.removeClass('navbar-fixed-top');
+        body.removeClass('padding-top');
     }
 });
